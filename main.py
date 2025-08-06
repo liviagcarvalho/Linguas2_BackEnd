9a40346e7065299fe3badf33255c1aa376e7a2c2
Lívia Carvalho <liviagc@al.insper.edu.br>
2025-08-05 22:06:24 -0300

    fix: coneccao com mongodb

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from mongoengine import connect
+# from models.user import User
 
 app = FastAPI()
+
+
+connect(db="LinguasII", host="mongodb+srv://liviagcarvalho:12345@cluster.sfj2axl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster")
 
 # Permitir conexão com o React frontend
 app.add_middleware(
@@ -11,3 +15,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+@app.get("/")
+def read_root():
+    return {"message": "API da Linguas2 rodando com sucesso!"}
